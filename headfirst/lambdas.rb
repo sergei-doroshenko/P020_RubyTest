ec42d5b0294f81b645cbc8a393ddca4270612049
Sergei <6823298@gmail.com>
2016-05-14 12:45:41 +0300

    Removed old files

--- headfirst/lambdas.rb
+++
@@ -1,6 +0,0 @@
-def calculation(a, b, operation)
-  operation.call(a, b)
-end
-
-puts format("5 + 6 = %i", calculation(5, 6, lambda { |a, b| a + b }) )# addition
-puts format("5 - 6 = %i", calculation(5, 6, lambda { |a, b| a - b }) )# subtraction
